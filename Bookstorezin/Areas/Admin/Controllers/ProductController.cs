ebda377fdeb7de944e2772bca7f4389b872208a8
zinmyoswe <overmidnight12@gmail.com>
2024-10-27 14:38:25 +0700

    Product CRUD and views

---
+++ Bookstorezin/Areas/Admin/Controllers/ProductController.cs
@@ -0,0 +1,119 @@
+﻿using Bookstore.DataAccess.Repository.IRepository;
+using Bookstore.Models;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Bookstorezin.Areas.Admin.Controllers
+{
+    [Area("Admin")]
+    public class ProductController : Controller
+    {
+        private readonly IUnitOfWork _unitOfWork;
+
+
+        public ProductController(IUnitOfWork unitOfWork)
+        {
+
+            _unitOfWork = unitOfWork;
+        }
+        public IActionResult Index()
+        {
+            List<Product> objProductList = _unitOfWork.Product.GetAll().ToList();
+            return View(objProductList);
+        }
+
+        public IActionResult Create()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public IActionResult Create(Product obj)
+        {
+            
+
+            if (ModelState.IsValid)
+            {
+                _unitOfWork.Product.Add(obj);
+                _unitOfWork.Save();
+                TempData["success"] = "Product created successfully";
+                return RedirectToAction("Index");
+            }
+            return View();
+
+        }
+
+        public IActionResult Edit(int? id)
+        {
+            if (id == null || id == 0)
+            {
+                return NotFound();
+            }
+            Product? ProductFromDb = _unitOfWork.Product.Get(u => u.Id == id);
+            //Product? ProductFromunitOfWork1 = _unitOfWork.Categories.FirstOrDefault(x=>x.Id == id);
+            //Product? ProductFromunitOfWork2 = _unitOfWork.Categories.Where(x=>x.Id == id).FirstOrDefault();
+
+            if (ProductFromDb == null)
+            {
+                return NotFound();
+            }
+            return View(ProductFromDb);
+        }
+
+        [HttpPost]
+        public IActionResult Edit(Product obj)
+        {
+
+            
+
+            if (ModelState.IsValid)
+            {
+                _unitOfWork.Product.Update(obj);
+                _unitOfWork.Save();
+                TempData["success"] = "Product Updated successfully";
+                return RedirectToAction("Index");
+            }
+            return View();
+
+        }
+
+
+        public IActionResult Delete(int? id)
+        {
+            if (id == null || id == 0)
+            {
+                return NotFound();
+            }
+            Product? ProductFromDb = _unitOfWork.Product.Get(u => u.Id == id);
+            //Product? ProductFromunitOfWork1 = _unitOfWork.Categories.FirstOrDefault(x=>x.Id == id);
+            //Product? ProductFromunitOfWork2 = _unitOfWork.Categories.Where(x=>x.Id == id).FirstOrDefault();
+
+            if (ProductFromDb == null)
+            {
+                return NotFound();
+            }
+            return View(ProductFromDb);
+        }
+
+        [HttpPost, ActionName("Delete")]
+        public IActionResult DeletePOST(int? id)
+        {
+            Product? obj = _unitOfWork.Product.Get(u => u.Id == id);
+
+            if (obj == null)
+            {
+                return NotFound();
+            }
+
+
+            _unitOfWork.Product.Remove(obj);
+            _unitOfWork.Save();
+            TempData["success"] = "Product deleted successfully";
+            return RedirectToAction("Index");
+
+
+
+        }
+
+
+    }
+}
