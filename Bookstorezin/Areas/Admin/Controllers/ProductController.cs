3e2ec037746efd2c8dca80bf22619b46212d893b
zinmyoswe <overmidnight12@gmail.com>
2024-10-28 15:42:05 +0700

    Add and Load Product DataTable

--- Bookstorezin/Areas/Admin/Controllers/ProductController.cs
+++ Bookstorezin/Areas/Admin/Controllers/ProductController.cs
@@ -142,9 +142,17 @@ namespace Bookstorezin.Areas.Admin.Controllers
             TempData["success"] = "Product deleted successfully";
             return RedirectToAction("Index");
 
-
-
         }
+
+        #region API CALLS
+
+        [HttpGet]
+        public IActionResult GetAll()
+        { 
+            List<Product> objProductList = _unitOfWork.Product.GetAll(includeProperties: "Category").ToList();
+            return Json(new { data = objProductList });
+        }
+        #endregion
 
 
     }
