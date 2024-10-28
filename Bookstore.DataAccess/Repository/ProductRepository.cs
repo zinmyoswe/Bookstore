77170be1ec5869fec63a2ddb7ccc153417452a33
zinmyoswe <overmidnight12@gmail.com>
2024-10-28 14:43:46 +0700

    Update Image

--- Bookstore.DataAccess/Repository/ProductRepository.cs
+++ Bookstore.DataAccess/Repository/ProductRepository.cs
@@ -21,7 +21,25 @@ namespace Bookstore.DataAccess.Repository
 
         public void Update(Product obj)
         {
-            _db.Products.Update(obj);
+       
+
+            var objFromDb = _db.Products.FirstOrDefault(u => u.Id == obj.Id);
+            if (objFromDb != null)
+            {
+                objFromDb.Title = obj.Title;
+                objFromDb.ISBN = obj.ISBN;
+                objFromDb.Description = obj.Description;
+                objFromDb.Price = obj.Price;
+                objFromDb.Price50 = obj.Price50;
+                objFromDb.ListPrice = obj.ListPrice;
+                objFromDb.Price100 = obj.Price100;
+                objFromDb.CategoryId = obj.CategoryId;
+                objFromDb.Author = obj.Author;
+                if (obj.ImageUrl != null)
+                {
+                    objFromDb.ImageUrl = obj.ImageUrl;  
+                }
+            }
         }
     }
 }
--- Bookstorezin/Areas/Admin/Controllers/ProductController.cs
+++ Bookstorezin/Areas/Admin/Controllers/ProductController.cs
@@ -63,6 +63,18 @@ namespace Bookstorezin.Areas.Admin.Controllers
                     string fileName = Guid.NewGuid().ToString() + Path.GetExtension(file.FileName);
                     string productPath = Path.Combine(wwwRootPath, @"images\product");
 
+                    if(!string.IsNullOrEmpty(productVM.Product.ImageUrl))
+                    {
+                        //delete the old image
+                        var oldImagePath =
+                            Path.Combine(wwwRootPath, productVM.Product.ImageUrl.TrimStart('\\'));
+
+                        if(System.IO.File.Exists(oldImagePath))
+                        {
+                            System.IO.File.Delete(oldImagePath);
+                        }
+                    }
+
                     using (var fileStream = new FileStream(Path.Combine(productPath, fileName), FileMode.Create))
                     { 
                         file.CopyTo(fileStream);
@@ -72,7 +84,15 @@ namespace Bookstorezin.Areas.Admin.Controllers
                 }
                 
 
-                _unitOfWork.Product.Add(productVM.Product);
+                
+                if(productVM.Product.Id == 0)
+                {
+                    _unitOfWork.Product.Add(productVM.Product);
+                }
+                else
+                {
+                    _unitOfWork.Product.Update(productVM.Product);
+                }
                 _unitOfWork.Save();
                 TempData["success"] = "Product created successfully";
                 return RedirectToAction("Index");
