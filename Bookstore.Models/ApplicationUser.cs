b25659c6bd32bfdfd3e2fac4a32d14409b14165c
zinmyoswe <overmidnight12@gmail.com>
2024-10-29 11:04:50 +0700

    extend identity users

---
+++ Bookstore.Models/ApplicationUser.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Bookstore.Models
+{
+    public class ApplicationUser : IdentityUser
+    {
+        [Required]
+        public int Id { get; set; }
+
+        public string? StreetAddress { get; set; }
+
+        public string? City { get; set; }
+
+        public string? State { get; set; }
+
+        public string? PostalCode { get; set; }
+    }
+
+    
+}
