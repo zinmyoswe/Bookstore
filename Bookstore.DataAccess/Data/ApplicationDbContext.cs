34ac969ec52f12125392c09436c0cafb4d64de46
zinmyoswe <overmidnight12@gmail.com>
2024-11-20 16:17:55 +0700

    Add Shopping cart Model

--- Bookstore.DataAccess/Data/ApplicationDbContext.cs
+++ Bookstore.DataAccess/Data/ApplicationDbContext.cs
@@ -19,6 +19,8 @@ namespace Bookstore.DataAccess.Data
 
         public DbSet<Author> Authors { get; set; }
 
+        public DbSet<ShoppingCart> ShoppingCarts { get; set; }
+
         public DbSet<ApplicationUser> ApplicationUsers { get; set; }
 
 
